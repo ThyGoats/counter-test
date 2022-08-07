ddde5a82944d316b8319a2d3b71faa61cf49ad3e
ThyGoats <russellchan98@gmail.com>
2022-08-07 14:59:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-fepkty
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fepkty)
---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Counter Test</h1>`;
+
+const counter = document.getElementById('counter');
+const btn = document.querySelector('.clickables');
+console.log('clickables');
